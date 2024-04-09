9916567100b0f89bf4414ddae8ee7812798aa14f
Nikki Gowan <nikki.gowan@sjsu.edu>
2024-04-09 10:12:19 -0700

    Created Home.jsx, About.jsx

---
+++ web-lp/src/About.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const divStyle = {
+    color:'white'
+};
+
+class About extends React.Component {
+
+    render() {
+        return (
+            <div style={divStyle}>
+                <h2>About Page</h2>
+                <main>
+                    <p>Testing about page</p>
+                </main>
+            </div>
+        )//return
+    }//render
+}//About
+
+
+
+export default About;
